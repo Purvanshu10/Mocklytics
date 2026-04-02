daa170e7717681a1aedc4d3f239991e9a304722f
Purvanshu10 <purvanshujindal10@gmail.com>
2026-04-03 01:26:42 +0530

    added console logs

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -17,5 +17,5 @@ export const env = {
   GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   GROQ_API_KEY: process.env.GROQ_API_KEY,
   HUGGINGFACE_API_KEY: process.env.HUGGINGFACE_API_KEY,
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT,
 };
